13c8f4e9bfcc2bb61a684d30ded7db38f9c12812
Drake <prachoat@yahoo.com>
2018-03-21 18:49:39 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
 <html><body><h1>I'm the rapper DRAKE . . . Call my name  D  R  A  K  E</h1>    </body></html>
-<?phpecho "Hello World!";?> 
-<?phpecho "Hello World!";?> 
-<?phpecho "Hello World!";?> 
