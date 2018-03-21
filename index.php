9aa5961b5b342ae88d603ad4f50da282e4f3d81e
Drake <prachoat@yahoo.com>
2018-03-21 18:46:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 
 
 <!DOCTYPE html><html><body><h1>I'm the rapper DRAKE . . . Call my name  D  R  A  K  E</h1>    </body></html>
-
-<?php
-$imagepath="phpimages/ocp.jpg";
-
-$image=imagecreatefromjpeg( $imagepath);
-
-header('Content-Type: image/jpeg');
-
-imagejpeg($image); ?>
-
-
-
 <?phpecho "Hello World!";?> 
+
