d3e363c672439a0375d9408b81947342b3a28b47
Drake <prachoat@yahoo.com>
2018-03-22 17:01:33 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <html><body>
   <p> </p>
-  Name: <input type="text" name="name" value="<?php echo $name;?>">E-mail: <input type="text" name="email" value="<?php echo $email;?>">Website: <input type="text" name="website" value="<?php echo $website;?>">Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>Gender:<input type="radio" name="gender"<?php if (isset($gender) && $gender=="female") echo "checked";?>value="female">Female<input type="radio" name="gender"<?php if (isset($gender) && $gender=="male") echo "checked";?>value="male">Male
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <p> </p>
   <p> </p>
   <p> </p>
   <p> </p>
@@ -11,7 +15,7 @@
   <p> </p>
   <p> </p>
   <p> </p>
-  <p><img src='ocp.jpg' style='position:fixed;top:50px;left:0px;width:80%;height:80%;z-index:-1;'></p>
+  <p><img src='ocp.jpg' style='position:fixed;top:120px;left:0px;width:80%;height:80%;z-index:-1;'></p>
   <p> </p>
 </body>
 </html>
